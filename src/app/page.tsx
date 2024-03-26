039552eab341535097298488165d6f837c4dc5ae
Crave <bersorgertv@gmail.com>
2024-03-26 19:44:38 +0700

    feat: Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,36 @@ import { redirect } from "next/navigation";
 import { auth, signOut } from "./auth";
 import { getPosts } from "@/db/query";
 import Post from "./_components/post";
+import Tag from "./_components/tag";
+import SortBy from "./_components/sort";
 
-export default async function Home(props: { searchParams: Record<string, string> }) {
+export default async function Home(props: { searchParams: {
+  tag?: string | string[]
+  sort?: 'DESC' | 'ASC',
+  page?: string
+} }) {
   const session = await auth()
 
   if (!session) {
     redirect('/signin')
   }
 
-  const searchParams = new URLSearchParams(props?.searchParams)
-
   const limit = 10
-  const pageOffset = ((Number(searchParams.get('page')) || 1) - 1) * limit
+  const pageOffset = ((Number(props?.searchParams?.page) || 1) - 1) * limit
 
+  const tags = props?.searchParams?.tag ? Array.isArray(props?.searchParams?.tag) ? props?.searchParams?.tag : [props?.searchParams?.tag] : undefined
+  const hasTags = Boolean(tags) && Array.isArray(tags) && Boolean(tags.length)
   const posts = await getPosts({
     limit,
     offset: pageOffset,
-    sort: 'DESC',
-    tags: ['guava']
+    sort: props?.searchParams?.sort || 'DESC',
+    tags,
   })
-
-  console.log(JSON.stringify(posts, null, 2), 'posts')
 
   return (
     <>
-    <nav className="flex p-2 justify-end">
+    <nav className="flex p-2 justify-end items-center gap-4">
+        <span>{session?.user?.email}</span>
         <form action={async () => {
           'use server'
           await signOut()
@@ -40,13 +45,19 @@ export default async function Home(props: { searchParams: Record<string, string>
         <h2 className="text-lg mb-4 font-semibold">
           Posts
         </h2>
-        <label htmlFor="orderBy">
-          <select name="orderBy" id="orderBy" defaultValue={'DESC'} className="border border-[#ccc] rounded-xl">
-            <option>DESC</option>
-            <option>ASC</option>
-          </select>
-        </label>
+        <SortBy />
       </section>
+      {
+        hasTags && (
+          <section className="flex flex-wrap gap-4 py-4">
+            {
+              tags?.map((tag) => {
+                return <Tag key={tag} type='closable' value={tag} />
+              })
+            }
+          </section>
+        )
+      }
       
       <article className="flex flex-col gap-4 max-w-[1200px]">
         {posts?.map((post) => {
